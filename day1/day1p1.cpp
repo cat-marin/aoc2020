c0618052886b96cbb58f7774f465e70f74d158ea
cat-marin <25059103+cat-marin@users.noreply.github.com>
2020-12-01 12:43:30 -0500

    day1p1 complete

--- day1/day1p1.cpp
+++ day1/day1p1.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
 
 using namespace std;
 
-int main() {
-    fstream input_file;
-    input_file.open("input", ios::in);
-    if(!input_file) {
-        cout << "no input\n";
-    } else if(input_file) {
-        cout << "yes input\n";
-        input_file.close();
+int lines[200];
+int size = 0;
+
+void find_input() {
+    ifstream input_file("input");
+    if(input_file.is_open()) {
+        string line;
+        while(getline(input_file, line)) {
+            lines[size] = (stoi(line));
+            size++;
+        }
     }
-    return 0;
+    input_file.close();
+}
+
+int main() {
+    find_input();
+    int ans;
+    for(int i = 0; i < size; i++) {
+        for (int j = i+1; j < size; j++){
+            if(lines[i] + lines[j] == 2020) {
+                ans = lines[i] * lines[j];
+            }
+        }
+    }
+    cout << "part one: " << ans << endl;
 }
